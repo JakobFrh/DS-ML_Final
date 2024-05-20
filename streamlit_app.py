76a5d737cf1e6358146689dad68c18c828aafbf3
JakobFrh <152393307+JakobFrh@users.noreply.github.com>
2024-05-20 22:42:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import CamembertTokenizer, CamembertForSequenceClassification
 import torch
-pip install sentencepiece
+!pip install sentencepiece
 
 # Load model and tokenizer
 model_dir = "model"
